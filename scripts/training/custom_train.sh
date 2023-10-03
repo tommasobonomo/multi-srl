cef2325d0aaed6ebc5865756372c4a3961abfdd9
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2023-10-03 13:43:48 +0200

    Training bash script

--- scripts/training/custom_train.sh
+++ scripts/training/custom_train.sh
@@ -4,12 +4,8 @@ export PYTHONPATH=$(pwd)
 
 python scripts/training/trainer.py fit \
 --config configurations/conll2009/base.yaml \
---config configurations/conll2009/en/conll08-dep-roberta/roberta-base-ft.yaml
+--config configurations/conll2009/en/unified-roberta/roberta-large-ft.yaml 
 
 python scripts/training/trainer.py fit \
 --config configurations/conll2009/base.yaml \
---config configurations/conll2009/en/conll08-dep-roberta/roberta-base-ft-silver.yaml
-
-python scripts/training/trainer.py fit \
---config configurations/conll2009/base.yaml \
---config configurations/conll2009/en/universal-dep-roberta/roberta-base-ft.yaml
+--config configurations/conll2009/en/roberta/roberta-large-ft.yaml 
