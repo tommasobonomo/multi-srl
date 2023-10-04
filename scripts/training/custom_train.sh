0e242ee78ff3e8f80729c8956ec19a78bc8fa818
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2023-10-04 12:12:12 +0200

    Add low-resource configs and script

--- scripts/training/custom_train.sh
+++ scripts/training/custom_train.sh
@@ -4,8 +4,16 @@ export PYTHONPATH=$(pwd)
 
 python scripts/training/trainer.py fit \
 --config configurations/conll2009/base.yaml \
---config configurations/conll2009/en/unified-roberta/roberta-large-ft.yaml 
+--config configurations/conll2009/en/roberta/roberta-base-ft-10pct.yaml
 
 python scripts/training/trainer.py fit \
 --config configurations/conll2009/base.yaml \
---config configurations/conll2009/en/roberta/roberta-large-ft.yaml 
+--config configurations/conll2009/en/conll08-dep-roberta/roberta-base-ft-10pct.yaml
+
+python scripts/training/trainer.py fit \
+--config configurations/conll2009/base.yaml \
+--config configurations/conll2009/en/conll08-dep-roberta/roberta-base-ft-silver-10pct.yaml
+
+python scripts/training/trainer.py fit \
+--config configurations/conll2009/base.yaml \
+--config configurations/conll2009/en/universal-dep-roberta/roberta-base-ft-10pct.yaml
