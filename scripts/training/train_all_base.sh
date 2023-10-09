77dbaad53961d02846f5a10f937f2b9a1f453db6
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2023-10-10 01:20:22 +0200

    Write bash training script

---
+++ scripts/training/train_all_base.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Training all base models for all languages on CoNLL 2009"
+echo "Total of 24 models will be trained"
+
+for lang in ca cs de en es zh
+do
+    echo "Training in language ${lang}"
+
+    python scripts/training/trainer.py fit \
+        --config configurations/conll2009/base.yaml \
+        --config configurations/conll2009/${lang}/conll08-dep-xlm-roberta/xlm-roberta-base-ft.yaml
+
+    python scripts/training/trainer.py fit \
+        --config configurations/conll2009/base.yaml \
+        --config configurations/conll2009/${lang}/conll08-dep-xlm-roberta/xlm-roberta-base-ft-silver.yaml
+
+    python scripts/training/trainer.py fit \
+        --config configurations/conll2009/base.yaml \
+        --config configurations/conll2009/${lang}/universal-dep-xlm-roberta/xlm-roberta-base-ft.yaml
+
+    python scripts/training/trainer.py fit \
+        --config configurations/conll2009/base.yaml \
+        --config configurations/conll2009/${lang}/xlm-roberta/xlm-roberta-base-ft.yaml
